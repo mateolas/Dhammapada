2dc34722d9ca45574d0a20a958fe5724dcaa1a82
mateolas <mateusz.kowara@gmail.com>
2017-02-16 20:54:16 +0100

    Dhammapada PL - Beginnings

---
+++ app/src/main/java/com/studio/skyline/dhammapada/SpisTresci.java
@@ -0,0 +1,13 @@
+package com.studio.skyline.dhammapada;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpisTresci extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spis_tresci);
+    }
+}
